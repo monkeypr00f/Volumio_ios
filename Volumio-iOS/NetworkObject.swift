2d73aa3b915402850a044a07c81fbcf4f5ad0991
Federico <fsintu@gmail.com>
2016-10-22 11:52:00 +0200

    v 1.0.6

---
+++ Volumio-iOS/NetworkObject.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkObject.swift
+//  Volumio-iOS
+//
+//  Created by Federico Sintucci on 21/10/16.
+//  Copyright © 2016 Federico Sintucci. All rights reserved.
+//
+
+import UIKit
+
+class NetworkObject: NSObject {
+
+}
---
+++ Volumio-iOS/SelectPlayerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SelectPlayerTableViewCell.swift
+//  Volumio-iOS
+//
+//  Created by Federico Sintucci on 18/10/16.
+//  Copyright © 2016 Federico Sintucci. All rights reserved.
+//
+
+import UIKit
+
+class SelectPlayerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
