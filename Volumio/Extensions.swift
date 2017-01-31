3797ee030aa19e8a7a1fd7bd60f81d4d1a14fec8
Michael <mib@yamanote.de>
2017-01-31 22:26:42 +0100

    add collection extension

--- Volumio/Extensions.swift
+++ Volumio/Extensions.swift
@@ -21,17 +21,6 @@ extension String {
     }
 }
 
-// MARK: - Foundation
-
-extension Collection where Indices.Iterator.Element == Index {
-    
-    /// Returns the element at the specified index iff it is within bounds, otherwise nil.
-    subscript (safe index: Index) -> Generator.Element? {
-        return indices.contains(index) ? self[index] : nil
-    }
-
-}
-
 // MARK: - UIKit
 
 extension UIView {
