c0d9a2193412b6c2839f8c928d72f975e61d0d9b
Michael <mib@yamanote.de>
2017-01-31 23:00:33 +0100

    add String and UIView extensions

---
+++ Volumio/DefaultStringConvertible.swift
@@ -0,0 +1,9 @@
+//
+//  DefaultStringConvertible.swift
+//  Volumio
+//
+//  Created by Michael Baumgärtner on 01.02.17.
+//  Copyright © 2017 Federico Sintucci. All rights reserved.
+//
+
+import Foundation
---
+++ Volumio/String+.swift
@@ -0,0 +1,9 @@
+//
+//  String+.swift
+//  Volumio
+//
+//  Created by Michael Baumgärtner on 01.02.17.
+//  Copyright © 2017 Federico Sintucci. All rights reserved.
+//
+
+import Foundation
